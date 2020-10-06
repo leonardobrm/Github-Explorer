0b8270c52b648135133bc5a7fde9647fd273f281
leonardo <leonardo.brasil59@outlook.com>
2020-10-06 00:13:56 -0300

    :rocket: navigating between routs

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable camelcase */
 import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 import api from '../../services/api';
-
 import logoImg from '../../assets/Logo.svg';
 
 import { Title, Form, Repositories, Error } from './styles';
@@ -82,7 +82,10 @@ const Dashboard: React.FC = () => {
 
       <Repositories>
         {repositories.map(repository => (
-          <a key={repository.full_name} href="teste">
+          <Link
+            key={repository.full_name}
+            to={`repositories/${repository.full_name}`}
+          >
             <img
               src={repository.owner.avatar_url}
               alt={repository.owner.login}
@@ -92,7 +95,7 @@ const Dashboard: React.FC = () => {
               <p>{repository.description}</p>
             </div>
             <FiChevronRight size={20} />
-          </a>
+          </Link>
         ))}
       </Repositories>
     </>
